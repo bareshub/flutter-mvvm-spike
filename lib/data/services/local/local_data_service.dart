ed086ea59ff268e85288f40b72f2080b80e6188d
Marco <marco.baresi01@gmail.com>
2025-05-05 22:15:10 +0200

    Update local data service to load sample assets activities

--- lib/data/services/local/local_data_service.dart
+++ lib/data/services/local/local_data_service.dart
@@ -1,9 +1,14 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
+import 'package:flutter_mvvm_spike/config/assets.dart';
+import 'package:flutter_mvvm_spike/domain/models/activity/activity.dart';
 
 class LocalDataService {
-  // TODO mock services
+  Future<List<Activity>> getActivities() async {
+    final json = await _loadStringAsset(Assets.activities);
+    return json.map<Activity>(Activity.fromJson).toList();
+  }
 
   Future<List<Map<String, dynamic>>> _loadStringAsset(String asset) async {
     final localData = await rootBundle.loadString(asset);
