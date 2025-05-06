37fe5e2cbb4d92aa9bf24fbdbae80ec4c938eae2
Marco <marco.baresi01@gmail.com>
2025-05-06 19:04:54 +0200

    Add auth header provider to activity api client

--- lib/data/services/api/api_client.dart
+++ lib/data/services/api/api_client.dart
@@ -4,6 +4,8 @@ import 'dart:io';
 import 'package:flutter_mvvm_spike/domain/models/activity/activity.dart';
 import 'package:flutter_mvvm_spike/utils/result.dart';
 
+/// Adds the `Authentication` header to a header configuration.
+typedef AuthHeaderProvider = String? Function();
 typedef ClientFactory = HttpClient Function();
 
 class ApiClient {
@@ -16,6 +18,19 @@ class ApiClient {
   final int _port;
   final ClientFactory _clientFactory;
 
+  AuthHeaderProvider? _authHeaderProvider;
+
+  set authHeaderProvider(AuthHeaderProvider authHeaderProvider) {
+    _authHeaderProvider = authHeaderProvider;
+  }
+
+  Future<void> _authHeader(HttpHeaders headers) async {
+    final header = _authHeaderProvider?.call();
+    if (header != null) {
+      headers.add(HttpHeaders.authorizationHeader, header);
+    }
+  }
+
   Future<Result<List<Activity>>> getActivityByDestination(String ref) async {
     final client = _clientFactory();
     try {
@@ -24,7 +39,7 @@ class ApiClient {
         _port,
         '/destination/$ref/activity',
       );
-
+      await _authHeader(request.headers);
       final response = await request.close();
       if (response.statusCode == 200) {
         final stringData = await response.transform(utf8.decoder).join();
